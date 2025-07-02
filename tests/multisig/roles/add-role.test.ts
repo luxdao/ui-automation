2c07082dfd763b35059d0f1c4530330a74122ba9
cbajandas <chebajandas@gmail.com>
2025-07-02 12:13:32 -0700

    Adding some more tests for treasury and roles. More interaction.

---
+++ tests/multisig/roles/add-role.test.ts
@@ -0,0 +1,23 @@
+import { getTestDao } from '../../../config/test-daos';
+import { BaseSeleniumTest } from '../../base-selenium-test';
+import { By } from 'selenium-webdriver';
+import { pages } from '../../../config/pages';
+import { getBaseUrl, appendFlagsToUrl } from '../../test-helpers';
+
+const ROLES_PATH = `${pages['roles']}?dao=${getTestDao('multisig').value}&demo_mode=on`;
+
+const test = new BaseSeleniumTest('roles', 'add-role');
+BaseSeleniumTest.run(async (test) => {
+  await test.start();
+  // Load the roles page
+  await test.driver!.get(appendFlagsToUrl(getBaseUrl() + ROLES_PATH));
+  // Click the button with text "Edit Roles"
+  const editRolesBtn = await test.waitForElement(By.xpath("//button[contains(., 'Edit Roles')]"), 10000);
+  await editRolesBtn.click();
+  // Click the button with text "Add Role"
+  const addRoleBtn = await test.waitForElement(By.xpath("//button[contains(., 'Add Role')]"), 10000);
+  await addRoleBtn.click();
+  // Wait for the input with data-testid="role-name"
+  await test.waitForElement(By.css('input[data-testid="role-name"]'), 10000);
+  console.log('Add Role flow loaded and role name input found.');
+}, test);
