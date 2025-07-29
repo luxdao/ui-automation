298667a10e13fc19a8fa4c2755e5e62d0bf2104e
cbajandas <cbajandas@decentdao.org>
2025-07-29 09:24:29 -0700

    This governance tab test will be the death of me. Trying out a different click method. Each time I address this flakiness, I get it working locally, then it fails in CI, then when I run it locally, it fails locally. Fingers crossed this is the change that ends my suffering.

--- tests/multisig/settings/governance-tab.test.ts
+++ tests/multisig/settings/governance-tab.test.ts
@@ -4,21 +4,30 @@ import { BaseSeleniumTest } from '../../base-selenium-test';
 import { By } from 'selenium-webdriver';
 import { pages } from '../../../config/pages';
 
+/* This test is extremely flaky for some reason. It seems identical to the token-voting test, but it struggles
+to click the governance tab. Most failures have been when the test thinks that the tab has been clicked, but
+the tab contents have not loaded. There have been many attempts at addressing this, but the underlying issue
+seems to have to do with the modal/overlay resulting in clicks being intercepted by other elements. */
+
 const test = new BaseSeleniumTest('settings', 'governance-tab');
 BaseSeleniumTest.run(async (test) => {
   // Load the DAO homepage
   await test.start();
   const daoHomePath = `${pages['dao-homepage']}?dao=${getTestDao('multisig').value}`;
   await test.driver!.get(appendFlagsToUrl(getBaseUrl() + daoHomePath));
-  // Scroll is required because there's a conflict with underlying elements. In this case, the 'Parent' button on the DAO homepage.
-  await test.driver!.executeScript("window.scrollBy(0, 100);");
-  await test.driver!.sleep(500); // Small delay to allow scroll animation
   // Click the 'Manage DAO' button
   const manageBtn = await test.waitForElement(By.css('[aria-label="Manage DAO"]'));
   await manageBtn.click();
   // Click on the 'Governance' tab using JavaScript to bypass click interception
   const governanceTab = await test.waitForElement(By.xpath("//p[text()='Governance']"));
-  await test.driver!.executeScript("arguments[0].click();", governanceTab);
+
+  // Try using the actions API to click the tab
+  const actions = test.driver!.actions({ bridge: true });
+  await actions.move({ origin: governanceTab }).click().perform();
+
+  // This is the method that should work, as it works with token-voting
+  // await test.driver!.executeScript("arguments[0].click();", governanceTab);
+
   // Wait for the signer input with value starting with "0x"
   await test.waitForElement(By.css('input[value^="0x"]'));
   console.log('Governance tab clicked and signer address found.');
